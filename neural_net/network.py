3b55506fdd2236600d802e180b7b2b5c45d8efb5
Oliver Jones <oliverxjoness@gmail.com>
2026-05-29 16:55:01 -0700

    Network layer functions

--- neural_net/network.py
+++ neural_net/network.py
@@ -1,12 +1,13 @@
 import numpy as np
-
+from layers import *
 
 class Network:
 
-    def __init__(layers):
+    def __init__(self, layers):
         # List of layers like:
         # [Linear(784,256), ReLU(), Linear(256,10), Softmax()]
-        return 0
+        # each one is a class
+        self.layers = layers
     
     def forward(x):
         # Loop through layers in order
@@ -14,14 +15,24 @@ class Network:
         # return final output
         return 0
     
-    def backward(grad):
+    def backward(self, grad):
         # Loop through layers in reverse order
         # pass layer's gradients as the input to previous one
-        return 0
+        gradient_current = grad
+        for layer in self.layers.reverse():
+            # update next gradient to previous layers gradient
+            gradient_current = layer.backward(gradient_current)
     
-    def paramters():
+    def parameters(self):
         # Through layers, find each linear layer and return list of (weight, gradient) tuples
         # optimizer uses these to know the updates
-        return 0
+        params_list = []
+        for layer in self.layers:
+            # Check if we have a linear layer
+            if isinstance(layer, Linear):
+                params_list.append((layer.W, layer.grad_W))
+                params_list.append((layer.b, layer.grad_b))
+
+        return params_list
     
     
