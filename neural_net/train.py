1faf70e9e1cedf82f7907a4fb4e80bc857e0aba5
TheTureFADED <ztx4799@gmail.com>
2026-06-01 23:20:31 -0700

    feat: training function (before test)

--- neural_net/train.py
+++ neural_net/train.py
@@ -4,7 +4,6 @@ import optimizers as op
 import layers as layers
 import losses as loss_function
 
-
 def train(model, X, y, optimizer, loss_function, 
           epochs = 16, batch_size = 32):
     """
@@ -22,9 +21,36 @@ def train(model, X, y, optimizer, loss_function,
     return loss history for a plot.
     """
     ...
+    rng = np.random.default_rng()
+    n = X.shape[0]
+    Testsize = X.shape[1]
+    temp = X.copy()
+    for i in range(epochs):
+        # shuffle colums
+        rng.shuffle(temp, axis=1)
+        result = np.array_split(temp, (Testsize // 32) + 1, axis=1)
+        outp = []
+        loss = np.zeros[len(y)]
+        # this loop for each batch
+        for part in result:
+            cur_outp = model.forward(part)
+            outp.append(cur_outp) 
+            loss += loss_function.forward(cur_outp, y) 
+            '''comput the loss and adding all the loss from each batches '''
+            grad_out = loss_function.backward()
+            model.backward(grad_out)
+            op.step(model.parameters())
+
+        if (i % 10 == 0):
+            print(" this world has loss " + loss)
+        
+
+
+        
+
+
 
     
-    n = X.shape[0]
     
 
     
